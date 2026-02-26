76e258ba6b3780dc1ffbaf0804b0c532c43b57ac
LuisEduardoS23 <luisesoares4@gmail.com>
2026-02-26 20:56:10 -0300

    correção na chave estrangeira do CursoRepository

--- src/main/java/br/com/luisEduardo/ForumHub/repository/CursoRepository.java
+++ src/main/java/br/com/luisEduardo/ForumHub/repository/CursoRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface CursoRepository extends JpaRepository<Curso, Integer> {
+public interface CursoRepository extends JpaRepository<Curso, Long> {
 
 
     List<Curso> nome(String nome);
